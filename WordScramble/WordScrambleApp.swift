6c3c443137aba1f88e65287c749ca0d23f44c753
Kern Goretzky <kern.goretzky@gmail.com>
2025-03-21 10:32:14 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Kern Goretzky on 2025-03-21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordScrambleTests/WordScrambleTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleTests.swift
+//  WordScrambleTests
+//
+//  Created by Kern Goretzky on 2025-03-21.
+//
+
+import Testing
+@testable import WordScramble
+
+struct WordScrambleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
